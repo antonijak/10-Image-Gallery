e26c52dace28dc45af1957782ce7dd02d60c6c34
Antonija <antonija.kasum@gmail.com>
2018-10-08 11:35:05 +0300

    Revert "Bugs resolved"
    This reverts commit 7e75f91f8cdcd0764d1709853df443bde24ccffb.

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ let main = document.querySelector('main');
 
 function displayPicture() {
   for (i = 0; i < photosInfo.length; i++) {
-    if (photosInfo[i].src.length > 0) {
+
+    if (photosInfo[i].src) {
+
       let picture = document.createElement('img');
       let modal = document.createElement('div');
       let details = document.createElement('div');
@@ -25,14 +27,14 @@ function displayPicture() {
       details.appendChild(noPic)
       details.appendChild(nextButton);
       pictureContainer.className = 'picture-container';
-
+      
       picture.className = 'picture';
       name.className = 'detail';
       nextButton.id = i;
       fullName.id = 'full-name';
 
       modal.className = 'modal';
-      modal.id = i;
+      modal.id= i;
       details.id = 'details';
       bigPicture.id = 'big-picture';
       name.id = 'fullName';
@@ -82,39 +84,18 @@ function displayPicture() {
 
 
         nextButton.addEventListener('click', (e) => {
-
-          let buttonId = parseInt(e.target.getAttribute('id')) + 1;
-
-
-          let buttonIdString = buttonId.toString();
-
-          let x = document.getElementById(buttonIdString);
-
-          console.log(x);
-
-          while (x === null) {
-            buttonId++;
-            
-
-            if (buttonId > photosInfo.length) {
-              buttonId = 0;
-            }
-            buttonIdString = buttonId.toString();
-
-            x = document.getElementById(buttonIdString);
-          }
-          x.style.display = 'block';
-          x.style.top = '0';
-
-
+          var buttonId = parseInt(e.target.getAttribute('id')) + 1;
+          let buttonIdString  = buttonId.toString();
+          var nextParsonModal = document.getElementById(buttonIdString);
+          nextParsonModal.style.display = 'block';
         })
-
+        
 
         iterator++
       }
-
+     
     }
-
   }
 }
 displayPicture();
+
